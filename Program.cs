d837ca30a058b50ab392331db52dc6d227ce723d
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-26 00:23:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingFormat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number...");
+
+            string  no = Console.ReadLine();
+            try
+            {
+                int num = int.Parse(no);
+                Console.WriteLine("Number is ...." + no);
+            }
+            catch(FormatException ex) {
+                Console.WriteLine("string format is wrong");
+            }
+            Console.ReadLine();
+        }
+    }
+}
